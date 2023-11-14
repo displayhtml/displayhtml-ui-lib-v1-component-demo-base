06a32f04aa3c2f23c79114b9f8d3d5a772040a21
Engineer <engineer@example.com>
2023-11-14 10:57:12 +0000

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <DHH1>H1 Element</DHH1>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.scss'
+import App from './App.vue'
+
+import '@displayhtml/displayhtml-libs-1/index.css'
+import dhlibs1 from '@displayhtml/displayhtml-libs-1'
+
+createApp(App).use(dhlibs1).mount('#app')
